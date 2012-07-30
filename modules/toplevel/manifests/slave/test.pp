2a443669a56147b74a0fa04da26fd837ce261f6f
Dustin J. Mitchell <dustin@mozilla.com>
2012-07-30 16:14:40 -0700

    Bug 778942: set resolution, but not when puppetizing

--- modules/toplevel/manifests/slave/test.pp
+++ modules/toplevel/manifests/slave/test.pp
@@ -1,6 +1,8 @@
 class toplevel::slave::test inherits toplevel::slave {
     # test slaves need to login so we can run graphical processes as a user
     include users::builder::autologin
+    # and need to have a well-known screen resolution for talos tests
+    include screenresolution::talos
     include ntp::atboot
 }
 
