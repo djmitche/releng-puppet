cd4c6b3ad576411eb8bd2f533902d2621b498f83
Dustin J. Mitchell <dustin@mozilla.com>
2014-07-18 12:30:31 -0400

    Bug 1040835: factor out exec with a semaphore into execonce
    ..and refactor firewall::windows_settings to match

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -240,6 +240,10 @@ node /mobile-imaging-stage1\.p127\.releng\.(scl1|scl3)\.mozilla\.com/ {
     users::root::extra_authorized_key {
         'mcote': ;
     }
+    shared::execonce {
+        'test-exec':
+            command => "/bin/touch /tmp/foo";
+    }
 }
 
 node /mobile-imaging-\d+\.p\d+\.releng\.(scl1|scl3)\.mozilla\.com/ {
