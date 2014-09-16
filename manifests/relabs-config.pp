51deed4a6d47f88248f6df2559fc996992b688a2
Dustin J. Mitchell <dustin@mozilla.com>
2014-09-16 15:43:48 -0400

    Bug 1065184: disable relabs upstream sync; a=orgonly

--- manifests/relabs-config.pp
+++ manifests/relabs-config.pp
@@ -24,7 +24,8 @@ class config inherits config::base {
     $puppet_server_reports = "http"
     $puppet_server_reporturl = "http://foreman.pvt.build.mozilla.org:3001/"
     $puppet_server_facturl = "http://foreman.pvt.build.mozilla.org:3000/"
-    $puppetmaster_upstream_rsync_source = 'rsync://puppetagain.pub.build.mozilla.org/data/'
+    ## disabled temporarily for bug 1065184
+    #$puppetmaster_upstream_rsync_source = 'rsync://puppetagain.pub.build.mozilla.org/data/'
     $puppetmaster_public_mirror_hosts = [ ]
     $puppetmaster_extsyncs = {
         'slavealloc' => {
